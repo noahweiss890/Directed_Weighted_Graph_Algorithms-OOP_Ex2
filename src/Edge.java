0034a96b8955b551b063d96903e95bfbf08e3393
Rashipachino <rashipachino@gmail.com>
2021-11-30 14:00:02 +0200

    Rashi First

---
+++ src/Edge.java
@@ -0,0 +1,53 @@
+import api.EdgeData;
+
+public class Edge implements EdgeData {
+
+    private int src;
+    private int dest;
+    private double weight;
+    private String info;
+    private int tag;
+
+    public Edge(int src, int dest, int weight){
+        this.src = src;
+        this.dest = dest;
+        this.weight = weight;
+        info = "";
+        tag = 0; // 0 = white for now, 1 is grey, 2 is black
+    }
+
+    @Override
+    public int getSrc() {
+        return src;
+    }
+
+    @Override
+    public int getDest() {
+        return dest;
+    }
+
+    @Override
+    public double getWeight() {
+        return weight;
+    }
+
+    @Override
+    public String getInfo() {
+        return info;
+    }
+
+    @Override
+    public void setInfo(String s) {
+        this.info = s;
+    }
+
+    @Override
+    public int getTag() {
+        return tag;
+    }
+
+    @Override
+    public void setTag(int t) {
+        this.tag = t;
+    }
+}
