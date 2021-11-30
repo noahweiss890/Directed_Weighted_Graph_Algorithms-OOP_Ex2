52981655a02ca13b825171978c8ffe5e479975e0
Rashipachino <rashipachino@gmail.com>
2021-11-30 14:53:48 +0200

    rashi second

---
+++ src/MyDirectedWeightedGraph.java
@@ -0,0 +1,72 @@
+import api.DirectedWeightedGraph;
+import api.EdgeData;
+import api.NodeData;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class MyDirectedWeightedGraph implements DirectedWeightedGraph {
+
+    private ArrayList<NodeData> nodes;
+
+
+    @Override
+    public NodeData getNode(int key) {
+        return nodes.get(key);
+    }
+
+    @Override
+    public EdgeData getEdge(int src, int dest) {
+        return null;
+    }
+
+    @Override
+    public void addNode(NodeData n) {
+        nodes.add(n.getKey(), n);
+    }
+
+    @Override
+    public void connect(int src, int dest, double w) {
+
+    }
+
+    @Override
+    public Iterator<NodeData> nodeIter() {
+        return null;
+    }
+
+    @Override
+    public Iterator<EdgeData> edgeIter() {
+        return null;
+    }
+
+    @Override
+    public Iterator<EdgeData> edgeIter(int node_id) {
+        return null;
+    }
+
+    @Override
+    public NodeData removeNode(int key) {
+        return null;
+    }
+
+    @Override
+    public EdgeData removeEdge(int src, int dest) {
+        return null;
+    }
+
+    @Override
+    public int nodeSize() {
+        return 0;
+    }
+
+    @Override
+    public int edgeSize() {
+        return 0;
+    }
+
+    @Override
+    public int getMC() {
+        return 0;
+    }
+}
